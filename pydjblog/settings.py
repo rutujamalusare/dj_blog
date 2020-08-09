f1f5bec047ea6da576cc6a685dbd80de97464e7a
rutuja <mrutuja24.rm@gmail.com>
2020-08-09 12:34:15 +0530

    staticfiles folder and file in it added

--- pydjblog/settings.py
+++ pydjblog/settings.py
@@ -120,8 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
